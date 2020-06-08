7013672e4d38f1804a26e1b53baa72e995c86e69
root <root@example.com>
2020-06-08 14:43:02 +0200

    the latest for save

---
+++ tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,11 @@
+// That's good that in test environment, no ApiKeys should be set
+// and no emails should be sent. ;)
+// But we could write something if we really want for tests.
+module.exports = {
+    setApiKey() {
+
+    },
+    send() {
+        
+    }
+}
